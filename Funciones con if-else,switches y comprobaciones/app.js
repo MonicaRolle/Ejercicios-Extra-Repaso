b2aadab40e17767e4d0334f17d7f3c43c950614f
Monica <monica.rolle13@gmail.com>
2021-03-01 11:10:50 +0100

    changes

---
+++ Funciones con if-else,switches y comprobaciones/app.js
@@ -0,0 +1,36 @@
+// Implementa una función que admita un parámetro, de cualquier tipo, y que compruebe si el parámetro es undefined o null .
+
+
+
+// Implementa una función que admita un número como parámetro y devuelva si el número es positivo o negativo.
+
+
+
+// Implementa una función que admita un número como parámetro y diga, por consola, si es mayor que 100, menor que 100 o exactamente 100.
+
+
+
+// Implementa una función que admita como parámetro un objeto y devuelva si dicho objeto tiene una propiedad ‘name’ o no.
+
+
+
+// Implementa una función que admita 2 números como argumento y compruebe si el primero es divisible por el segundo.
+
+
+// Implementa una función que admita un string y un número como parámetro, y comprobar que tienen ese número de caracteres.
+
+
+
+// Implementa una función que admita un día de la semana en formato número (del 1 al 7) y devuelva que día de la semana es (en texto).
+
+
+
+// Implementa un función que dado un número (del 1 al 12), diga a qué mes corresponde en texto.
+
+
+
+// Implementa una función que admita 2 arrays como argumento, y devuelva el array más largo.
+
+
+
+// Implementa una función que admita 2 arrays como argumento, y devuelva si el primer elemento de ambos arrays es igual o no.
